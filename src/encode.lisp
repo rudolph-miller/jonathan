22455b3634dd80f13d381f382f344d812a23379b
Matt Novenstern <fisxoj@gmail.com>
2016-10-09 16:13:27 -0400

    Add method for simple-string
    Fixes Rudolph-Miller/jonathan#40 by adding a method on `simple-string`
    and changing `string`'s method to use a slower way, but not error.

--- src/encode.lisp
+++ src/encode.lisp
@@ -207,8 +207,11 @@
 "Write obj as JSON string."
 (defgeneric %to-json (obj))
 
-(defmethod %to-json ((string string))
+(defmethod %to-json ((string simple-string))
   (string-to-json string))
+
+(defmethod %to-json ((string string))
+  (string-to-json (princ-to-string string)))
 
 (defmethod %to-json ((number number))
   (%write-string (princ-to-string number)))
