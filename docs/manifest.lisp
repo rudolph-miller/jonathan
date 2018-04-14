95c1e0dee9ad9b904ebeb19a568432484d3694e5
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2018-04-14 09:20:55 +0000

    update docs at 1523697655. [ci skip]

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:jonathan)
+ :documents ((:title "JONATHAN"
+              :authors ("Rudolph-Miller")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
